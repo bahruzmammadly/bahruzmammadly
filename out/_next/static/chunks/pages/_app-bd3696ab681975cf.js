441e612381861dd5d5062b2fc8baf4f7cb10cba6
user <user@localhost>
2026-02-13 21:55:38 +0400

    fix: postcss config and tailwind version mismatch

---
+++ out/_next/static/chunks/pages/_app-bd3696ab681975cf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{372:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8651)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(372),n(4531))),_N_E=_.O()}]);
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,1 @@
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-
-export default config;
+export default { plugins: { tailwindcss: {}, autoprefixer: {}, }, };
